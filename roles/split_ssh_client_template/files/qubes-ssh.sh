40b14a6d400de85bbbb825108142cf97789b6d43
Frédéric Pierret (fepitre) <frederic@invisiblethingslab.com>
2026-05-25 12:53:04 +0200

    Make shellcheck happy: add shell directive to split-SSH profile hook

--- roles/split_ssh_client_template/files/qubes-ssh.sh
+++ roles/split_ssh_client_template/files/qubes-ssh.sh
@@ -1,3 +1,5 @@
+# shellcheck shell=sh
+# Sourced from /etc/profile.d; no shebang.
 if [ -s /rw/config/split-ssh-default-agent ]; then
     QUBES_SSH_AGENT=$(cat /rw/config/split-ssh-default-agent)
     export QUBES_SSH_AGENT
